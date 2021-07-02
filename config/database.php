d8a61658006822195455cfd54439bb517ed39b41
emanhamdy a <62082405+emanhamdy-a@users.noreply.github.com>
2021-07-02 23:19:01 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-
+$DATABASE_URL=parse_url('DATABASE_URL');
 return [
 
     /*
@@ -64,18 +64,18 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'schema' => 'public',
-            'sslmode' => 'prefer',
+          'driver' => 'pgsql',
+          'url' => env('DATABASE_URL'),
+          'host' => isset($DATABASE_URL['host']) ? $DATABASE_URL['host'] : null,
+          'port' => isset($DATABASE_URL['port']) ? $DATABASE_URL['port'] : null,
+          'database' => isset($DATABASE_URL['path']) ? ltrim($DATABASE_URL['path'],"/") : null,
+          'username' => isset($DATABASE_URL['user']) ? $DATABASE_URL['user'] : null,
+          'password' => isset($DATABASE_URL['pass']) ? $DATABASE_URL['pass'] : null,
+          'charset' => 'utf8',
+          'prefix' => '',
+          'prefix_indexes' => true,
+          'schema' => 'public',
+          'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
